f65290f9c9067a3c30a658f2879a42514dbc81bb
Valde <randomvald0069@gmail.com>
2020-09-13 00:47:02 +0200

    refactored

---
+++ configs/polybar/load.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+POLY=$HOME/.config/polybar
+
+mkdir -p POLY
+cp -r ./* POLY
--- scripts/desktop_startup.sh
+++ scripts/desktop_startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-bash set_desktop_monitors.sh
+#bash set_desktop_monitors.sh
 nvidia-settings &
 sleep 3
 killall -9 nvidia-settings

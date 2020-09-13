66d4cf3c4822bdc6e317ac20088362d96722d53b
Valde <randomvald0069@gmail.com>
2020-09-13 17:43:26 +0200

    bg and workspace move

--- configs/i3/install.sh
+++ configs/i3/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo add-apt-repository ppa:kgilmer/speed-ricer
 sudo apt-get update
-sudo apt-get install -y i3-gaps picom
+sudo apt-get install -y i3-gaps picom feh
---
+++ configs/i3/wp.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+feh --bg-scale bg.jpg
+#feh bg.jpg

7549a44c19fa2c3ad08218a43f7626ff25e7dd33
Valdemar <randomvald0069@gmail.com>
2020-09-15 17:00:17 +0200

    laptop poly

--- configs/polybar/launch.sh
+++ configs/polybar/launch.sh
@@ -2,6 +2,17 @@
 
 if pgrep polybar; then killall -9 polybar; fi
 
+if xrandr --query | grep " connected" | cut -d" " -f1 | grep eDP-1
+then
+    echo "lap";
+    if xrandr --query | grep " connected" | cut -d" " -f1 | grep DP-1
+    then
+        PRIMARY_MONITOR=DP-1
+    fi
+else
+    echo "desktop";
+fi
+
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
     MONITOR=$m polybar --reload center &

96937e4c4700892d917df818906354c5fb8bb3cc
ValdemarGr <randomvald0069@gmail.com>
2021-11-08 14:12:45 +0100

    launch enhancement

--- configs/polybar/launch.sh
+++ configs/polybar/launch.sh
@@ -2,24 +2,27 @@
 
 if pgrep polybar; then killall -9 polybar; fi
 
-if xrandr --query | grep " connected" | cut -d" " -f1 | grep "^HDMI-0$"
-then
-    PRIMARY_MONITOR=DP-2
-else
-    if xrandr --query | grep " connected" | cut -d" " -f1 | grep "^eDP-1$"
-    then
-        echo "lap";
-        if xrandr --query | grep " connected" | cut -d" " -f1 | grep "^DP-1$"
-        then
-            PRIMARY_MONITOR=DP-1
-        else
-            PRIMARY_MONITOR=eDP-1
-        fi
-    else
-        PRIMARY_MONITOR=DP-4
-        echo "desktop";
-    fi
+if [[ -z  "${PRIMARY_MONITOR}" ]]; then
+  if xrandr --query | grep " connected" | cut -d" " -f1 | grep "^HDMI-0$"
+  then
+      PRIMARY_MONITOR=DP-2
+  else
+      if xrandr --query | grep " connected" | cut -d" " -f1 | grep "^eDP-1$"
+      then
+          echo "lap";
+          if xrandr --query | grep " connected" | cut -d" " -f1 | grep "^DP-1$"
+          then
+              PRIMARY_MONITOR=DP-1
+          else
+              PRIMARY_MONITOR=eDP-1
+          fi
+      else
+          PRIMARY_MONITOR=DP-4
+          echo "desktop";
+      fi
+  fi
 fi
+
 echo "running with $PRIMARY_MONITOR as primary"
 
 if type "xrandr"; then

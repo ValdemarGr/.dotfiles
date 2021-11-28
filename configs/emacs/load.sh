d36cf90bf71f4f3ee25a0bf671d9ac9c042b6dde
Valdemar Grange <randomvald0069@gmail.com>
2021-11-28 15:03:29 +0100

    added emacs init

--- configs/emacs/load.sh
+++ configs/emacs/load.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 cp -r .emacs.d ~
+cp emacs.service ~/.config/systemd/user/emacs.service
+systemctl enable --user emacs
+systemctl start --user emacs
+cp emacsclient.desktop ~/.local/share/applications

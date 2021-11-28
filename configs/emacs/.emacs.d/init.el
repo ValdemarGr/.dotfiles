09e3fd62bf9321995a48608bdcf4f776778d3498
Valdemar Grange <randomvald0069@gmail.com>
2021-11-28 20:09:03 +0100

    work on emacs

--- configs/emacs/.emacs.d/init.el
+++ configs/emacs/.emacs.d/init.el
@@ -16,7 +16,13 @@
 (setq package-enable-at-startup nil)
 ; straight.el end
 
-(setq packages '(magit helm))
+(setq packages '(magit helm helm-themes projectile helm-projectile treemacs
+		       ace-window helm-ag avy 
+		       ivy swiper expand-region
+		       company flycheck
+		       lsp-mode lsp-ui rescript-mode lsp-rescript
+		       cherry-blossom-theme color-theme-sanityinc-tomorrow
+		       ))
 
 (dolist (package packages)
   (straight-use-package package))
@@ -28,3 +34,42 @@
 (global-set-key (kbd "M-x") 'helm-M-x)
 ; helm-mode 1)				
 ; helm end
+
+(menu-bar-mode -1)
+(toggle-scroll-bar -1)
+(tool-bar-mode -1)
+
+; projectile begin
+(projectile-mode +1)
+
+(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+
+(setq projectile-project-search-path '(("~/git" . 1)))
+
+(add-to-list 'default-frame-alist
+	     '(font . "FiraCode Nerd Font Mono-12"))
+; projectile end
+
+; avy begin
+(global-set-key (kbd "C-:") 'avy-goto-char)
+(global-set-key (kbd "C-'") 'avy-goto-char-2)
+(global-set-key (kbd "M-g f") 'avy-goto-line)
+(global-set-key (kbd "M-g w") 'avy-goto-word-1)
+(global-set-key (kbd "M-g e") 'avy-goto-word-0)
+; avy end
+
+
+
+(load-theme 'sanityinc-tomorrow-bright t)
+
+(customize-set-variable
+  'lsp-rescript-server-command
+    '("node" "/home/valde/rescript/extension/server/out/server.js" "--stdio"))
+(with-eval-after-load 'rescript-mode
+  ;; Tell `lsp-mode` about the `rescript-vscode` LSP server
+  (require 'lsp-rescript)
+  ;; Enable `lsp-mode` in rescript-mode buffers
+  (add-hook 'rescript-mode-hook 'lsp-deferred)
+  ;; Enable display of type information in rescript-mode buffers
+  (require 'lsp-ui)
+  (add-hook 'rescript-mode-hook 'lsp-ui-doc-mode))
--- configs/emacs/load.sh
+++ configs/emacs/load.sh
@@ -2,5 +2,9 @@
 cp -r .emacs.d ~
 cp emacs.service ~/.config/systemd/user/emacs.service
 systemctl enable --user emacs
+systemctl stop --user emacs || 0
 systemctl start --user emacs
 cp emacsclient.desktop ~/.local/share/applications
+mkdir ~/rescript
+curl -L https://github.com/rescript-lang/rescript-vscode/releases/download/1.1.3/rescript-vscode-1.1.3.vsix --output ~/rescript/ext.zip
+unzip ~/rescript/ext.zip -d ~/rescript

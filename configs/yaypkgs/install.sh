be6524f8cb9ba4d4966e3fa59bade68d4c876f3b
Valdemar Grange <randomvald0069@gmail.com>
2021-11-28 01:09:03 +0100

    emacs pkg yay

--- configs/yaypkgs/install.sh
+++ configs/yaypkgs/install.sh
@@ -3,4 +3,4 @@ yay -S firefox i3 lightdm lightdm-gtk-greeter pulseaudio rofi rofimoji neovim-ni
   tmux feh python-pip curl netcat xorg-xrandr xorg-server bat fd ripgrep zoxide pavucontrol coursier github-cli \
   adobe-source-code-pro-fonts fontconfig cantarell-fonts gnu-free-fonts gsfonts libfontenc libxfont2 noto-fonts \
   noto-fonts-emoji terminus-font xorg-fonts-encodings ttf-dejavu ttf-roboto ttf-roboto-mono terraform-ls \
-  google-cloud-sdk jome-git bazelisk docker docker-compose alacritty
+  google-cloud-sdk jome-git bazelisk docker docker-compose alacritty emacs

b4d85889252c0eb734d841a31008e5ab2b472df2
ValdemarGr <randomvald0069@gmail.com>
2023-09-12 14:28:15 +0200

    ts

--- configs/nvim/lua/valde/treesitter.lua
+++ configs/nvim/lua/valde/treesitter.lua
@@ -1,10 +1,23 @@
+
+local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+parser_config.authzed = {
+        install_info = {
+                url = "https://github.com/mleonidas/tree-sitter-authzed", -- local path or git repo
+                files = { "src/parser.c" },
+                generate_requires_npm = false,
+                requires_generate_from_grammar = false,
+                -- optional entries:
+                branch = "main", -- default branch in case of git repo if different from master
+        },
+        filetype = "authzed", -- if filetype does not match the parser name
+}
+
 require("nvim-treesitter.configs").setup{
-  ensure_installed = { "scala", "graphql" },
+  ensure_installed = { "scala", "graphql", "authzed" },
   highlight = {
     enable = true
   }
 }
-
 -- vim.cmd("TSEnable hightlight scala")
 
 -- local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
